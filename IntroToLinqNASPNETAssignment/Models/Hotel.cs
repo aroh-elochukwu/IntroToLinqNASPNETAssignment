544a8da150c1df62042d796d50c46af701622178
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-07-06 18:33:13 -0500

    complete question 2 criteria

--- IntroToLinqNASPNETAssignment/Models/Hotel.cs
+++ IntroToLinqNASPNETAssignment/Models/Hotel.cs
@@ -42,7 +42,7 @@
             }
         }
 
-        public Room GetRoom(string roomNumber)
+        public Room GetRoom(int roomNumber)
         {
             try
             {
@@ -101,7 +101,7 @@
             }
         }
 
-        public Reservation ReserveRoom(int clientID, int occupants, string roomNumber, DateTime reservationDate)
+        public Reservation ReserveRoom(int clientID, int occupants, int roomNumber, DateTime reservationDate)
         {           
             Client client = GetClient(clientID);
 
@@ -118,6 +118,105 @@
                 return null;
             }
         }
+
+        public void CheckIn(string clientName)
+        {
+            try
+            {
+                Client presentClient = Clients.First(theClient => theClient.Name == clientName);
+
+                Reservation clientReservation = presentClient.Reservations.First(theReservation => theReservation.StartDate == DateTime.Today);
+                clientReservation.Room.IsOccupied = true;                
+            }
+            catch(NullReferenceException)
+            {
+                
+            }
+        }
+
+        public void CheckOutRoom(int roomNumber)
+        {
+            try
+            {
+               Room theRoom = Rooms.First(room => room.Number == roomNumber);
+                theRoom.IsOccupied = false;
+            }
+            catch(NullReferenceException)
+            {
+
+            }
+        }
+
+        public void CheckOutRoom(string clientName)
+        {
+            try
+            {
+                Client presentClient = Clients.First(theClient => theClient.Name == clientName);
+                Reservation clientReservation = presentClient.Reservations.First(theReservation => theReservation.StartDate == DateTime.Today);
+                clientReservation.Room.IsOccupied = false;
+
+            }
+            catch (NullReferenceException)
+            {
+
+            }
+
+        }
+
+        //I am having difficulty agreeing with the questions description of a room's occupancy
+        //I think TotalCapacityRemaining() looks better like this
+        public int TotalCapacityRemaining()
+        {
+            try
+            {
+               return Rooms.Where(room => room.IsOccupied == false).ToList().Count();
+
+            }
+            catch (NullReferenceException)
+            {
+                return 0;
+            }  
+
+        }
+
+        public int AverageOccupancyPercentage()
+        {
+            try
+            {
+               List<Room> occupiedRooms = Rooms.Where(room => room.IsOccupied == true).ToList();
+                int presentOccupants = 0;
+                int occupiedRoomsFullCapacity = 0;
+
+                foreach(Room room in occupiedRooms)
+                {
+                    presentOccupants =+ room.Reservations.First(reservation => reservation.StartDate == DateTime.Today).Occupants;
+
+                    occupiedRoomsFullCapacity = +occupiedRooms.Capacity;
+                }
+
+                return (presentOccupants * 100 )/ occupiedRoomsFullCapacity ;
+
+            }
+            catch (NullReferenceException)
+            {
+                return 0;
+            }
+        }
+
+        public List<Reservation> FutureBookings()
+        {
+           
+            try
+            {
+                return Reservations.Where(reservation => reservation.StartDate > DateTime.Today).ToList();
+            }
+            catch(NullReferenceException)
+            {
+                return null;
+            }
+        }
+
+
     }
 }
 
--- IntroToLinqNASPNETAssignment/Models/Room.cs
+++ IntroToLinqNASPNETAssignment/Models/Room.cs
@@ -3,11 +3,11 @@
     public class Room
     {
         public int Id { get; set; } = 0;
-        public string Number { get; set; }
+        public int Number { get; set; }
         public int Capacity { get; set; }
         public bool IsOccupied { get; set; }
         public ICollection<Reservation> Reservations { get; set; }= new List<Reservation>();
-        public Room(string number, int capacity)
+        public Room(int number, int capacity)
         {
             Id = Id++;
             Number = number;
